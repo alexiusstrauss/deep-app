6da67605559cb331fb74cc9c5a89203281ef3377
Alexius Strauss <alexius.marques@gmail.com>
2022-03-21 21:17:29 -0300

    feat Add test and mock

--- src/service_storage/storages.py
+++ src/service_storage/storages.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from typing import List
 
@@ -5,18 +6,21 @@ from google.cloud import storage
 from service_storage.interfaces import StorageInterface
 
 
+logger = logging.getLogger(__name__)
+
 class GoogleStorage(StorageInterface):
     def __init__(self):
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "service_storage/credentials.json"
+        os.environ[
+            "GOOGLE_APPLICATION_CREDENTIALS"
+        ] = "service_storage/credentials.json"
+        self.client = storage.Client()
 
     def list_buckets(self) -> dict:
         """
         Lists all buckets
         """
-        storage_client = storage.Client()
-        buckets = storage_client.list_buckets()
+        buckets = self.client.list_buckets()
         return {bucket.name: bucket for bucket in buckets}
-
 
     def save_to_storage(
         self,
@@ -24,28 +28,26 @@ class GoogleStorage(StorageInterface):
         data: bytes,
         file_path: str,
         content_type: str = "application/pdf",
-    ):
+    ) -> str:
         """
         Saves a file to Google Cloud Storage
         default content type is application/pdf
-        Return: public url
+        Return: Blob or Error
         """
         bucket = self.get_or_create_bucket(bucket_name)
         blob = bucket.blob(file_path)
         try:
             blob.upload_from_string(data, content_type=content_type)
-            return blob
+            return blob.public_url
         except Exception as error:
-            print(error)
-            return None
+            logger.exception(f"Error saving file to storage: {error}")
+            raise error
 
     def get_or_create_bucket(self, bucket_name: str):
-        client_storage = storage.Client()
-
-        if storage.Bucket(client_storage, bucket_name.lower()).exists():
-            return client_storage.get_bucket(bucket_name.lower())
+        if storage.Bucket(self.client, bucket_name.lower()).exists():
+            return self.client.get_bucket(bucket_name.lower())
         else:
-            return client_storage.create_bucket(bucket_name.lower())
+            return self.client.create_bucket(bucket_name.lower())
 
     def delete_file(self, bucket_name: str, file_path: str):
         blob = self.get_file(bucket_name, file_path)
